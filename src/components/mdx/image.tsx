2c14f218f7187dcf58c9c930d3a5b583db64c4b7
Johnie Hjelm <johnie.hjelm@schibsted.com>
2025-11-13 20:47:13 +0100

    refactor: simplify MDXImage component by removing unused logic and props

--- src/components/mdx/image.tsx
+++ src/components/mdx/image.tsx
@@ -1,20 +1,5 @@
-import { type ComponentPropsWithoutRef, useRef } from 'react';
-
-let imageCount = 0;
+import type { ComponentPropsWithoutRef } from 'react';
 
 export function MDXImage(props: ComponentPropsWithoutRef<'img'>) {
-  const isFirstImage = useRef(imageCount === 0);
-
-  if (isFirstImage.current) {
-    imageCount += 1;
-  }
-
-  return (
-    <img
-      {...props}
-      alt={props.alt ?? ''}
-      fetchPriority={isFirstImage.current ? 'high' : undefined}
-      loading={isFirstImage.current ? 'eager' : 'lazy'}
-    />
-  );
+  return <img {...props} alt={props.alt ?? ''} loading="lazy" />;
 }
