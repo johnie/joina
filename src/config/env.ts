64d58e04e370447d2d1180377f27194da4759230
Johnie Hjelm <johnie.hjelm@schibsted.com>
2026-01-06 14:10:29 +0100

    fix: add BUILD_TIMESTAMP to environment configuration

--- src/config/env.ts
+++ src/config/env.ts
@@ -2,3 +2,7 @@ export const GIT_SHA = import.meta.env.VITE_GIT_SHA as string | undefined;
 export const GIT_SHA_URL = import.meta.env.VITE_GIT_SHA_URL as
   | string
   | undefined;
+
+export const BUILD_TIMESTAMP =
+  (import.meta.env.VITE_BUILD_TIME as string | undefined) ||
+  new Date().toISOString().split('T')[0];
